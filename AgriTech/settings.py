451392181eba93b68f3e40b9090865208f88ff71
Sahil Patel <sahildhanani2004@gmail.com>
2025-06-19 17:58:14 +0530

    Update settings.py

--- AgriTech/settings.py
+++ AgriTech/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'agriproject1310@gmail.com'
-EMAIL_HOST_PASSWORD = 'ncewxbywouxmurxl'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
